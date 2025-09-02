3fa966f96f52cdf2086bd62726164d927eb6eb0d
yuj-2 <tree9030@naver.com>
2025-09-02 17:29:06 +0900

    days12 ex07 수정

--- javaPro/src/days12/Ex07.java
+++ javaPro/src/days12/Ex07.java
@@ -1,14 +1,49 @@
 package days12;
+
 /**
- * @author yujin  
- * @date 2025. 9. 2. 오후 4:47:34 
+ * @author kenik  
+ * @date 2025. 9. 2. 오후 4:47:20 
  * @subject [다차원 배열]
  * @content  ㄴ 2차원 이상의 배열을 다차원 배열이라고 한다. 
  *              2차원, 3차원, 4차원... ( 메모리 허용 )
  *           ㄴ *** 배열의 배열이다. *** ( 중요한 개념, 암기 )   
  */ 
 public class Ex07 {
-	public static void main(String[] args) {
-		
-	}
-}
+
+   public static void main(String[] args) {
+      /*
+      // 1차원 배열 - 열
+      //int [] m = new int[8];
+      
+      // 2차원 배열 -       2행 4열 2차원 배열 m
+      // int [][] m = new int[2][4];
+      
+      // 3차원 배열          2면2행2열 
+      int [][][] m = new int[2][2][2];
+      
+      int [] kors = new int[30*10];
+      // 4반의 국어 평균     kors[90] ~ kors[119]
+      // 4반 11번 국어 점수  kors[100]    kors[3][10]
+      
+      //                       반   번호
+      //                      10행 30열
+      int [][] kors = new int[10][30];      
+      kors[3][0]~kors[3][29]
+      
+      // 30*10 *3
+      int [] kors = new int[3*10*30]; // 900
+      // 2학년 3반 국어 평균   kors[260]~ kors[289]
+      // 2학년 3반  11번 국어 kors[270]
+      
+      //                       학년 반  번호
+      //                        면 행 열
+      int [][][] kors = new int[3][10][30];
+      
+      // 4차원                     권 면 행 열 
+      int [][][][] kors = new int[3][10][30][5];
+      // 2학년 3반  11번 국어 kors[1][2][10]ㄹ
+      */ 
+
+   } // 
+
+} // 
